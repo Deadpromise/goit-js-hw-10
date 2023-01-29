448a8361229661448231e9bd069dc860c8051214
Dmitry Yavtushenko <rosengan22@gmail.com>
2023-01-30 00:31:49 +0200

    done

--- src/index.js
+++ src/index.js
@@ -18,28 +18,68 @@ inputRef.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 function onSearch(e) {
     userTyped = e.target.value.trim();
     if (userTyped === '') {
-        return alert('pusto');
+        clearListMarkup();
+        clearInfoMarkup();
+        return;
     }
-    fetchCountries(userTyped).then((result) => {
-        
+    fetchCountries(userTyped).then((result) => {   
         if (result.length > MAX_COUNT) {
             Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+            clearListMarkup();
+            clearInfoMarkup();
             return;
-        } else if (result.length >= MIN_COUNT || result.length <= MAX_COUNT) {
-            console.log('ot 2 do 10');
+        } else if (result.length >= MIN_COUNT && result.length <= MAX_COUNT) {
+            // console.log('ot 2 do 10');
+            clearInfoMarkup();
             buildFewCountries(result);
-        };
-        console.log(result);
-    }).catch()
+            return;
+        } else {
+            clearListMarkup();
+            buildOneCountry(result);
+            return;
+        }
+    }).catch(onFetchError)
 };
 
 function buildFewCountries(arr) {
+    clearListMarkup();
     const markUp = arr.map(el => {
-       return `<li class="country-item"><img class="country-flag" src="${el.flags.svg}" alt="${el.name.official} flag"><p>${el.name.official}</p></li>`
+        return `<li class="country-item"><img class="country-list-flag" src="${el.flags.svg}" alt="${el.name.official} flag">
+       <p class="country-list-name">${el.name.official}</p></li>`
         }).join('');
     // console.log(markUp);
     countryListRef.insertAdjacentHTML('beforeend', markUp);
 }
 
+function clearListMarkup() {
+    countryListRef.innerHTML = '';
+};
 
-//  console.log(el.name.official);
+function buildOneCountry(arr) {
+    clearInfoMarkup();
+    const markUp = arr.map(el => {
+        const countryLang = Object.values(el.languages).join(', ');
+    
+        return `<div class="country-title">
+        <img class="country-solo-flag" src="${el.flags.svg}" alt="${el.name.official} flag">
+        <p class="country-solo-name">${el.name.official}</p></div>
+        <ul>
+        <li class="country-solo-item">Capital:<span class="country-solo-span">${el.capital}</span></li>
+        <li class="country-solo-item">Population:<span class="country-solo-span">${el.population}</span></li>
+        <li class="country-solo-item">Languages:<span class="country-solo-span">${countryLang}</span></li>
+        </ul>`
+    }).join('');
+
+    countryInfoRef.insertAdjacentHTML('beforeend', markUp);
+};
+
+function clearInfoMarkup() {
+    countryInfoRef.innerHTML = '';
+};
+
+function onFetchError() {
+    Notiflix.Notify.failure('Oops, there is no country with that name');
+    clearListMarkup();
+    clearInfoMarkup();
+    return;   
+};
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,12 +1,5 @@
-import Notiflix from 'notiflix';
-
-
 export default function fetchCountries(inputData) {
-    return fetch(`https://restcountries.com/v3.1/name/${inputData}?fields=name,capital,population,flags,languages`).then(res => {
-    if (!res.ok) {
-        Notiflix.Notify.failure('Oops, there is no country with that name');
-        return;
-        };   
+    return fetch(`https://restcountries.com/v3.1/name/${inputData}?fields=name,capital,population,flags,languages`).then(res => { 
     return res.json();
     })
 };
